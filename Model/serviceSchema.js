0614cc813cb561215d56732e3ced546c91f91487
Rahinmon2903 <rahin2903@gmail.com>
2025-12-03 09:48:43 +0530

    user and sevice schema completed

---
+++ Model/serviceSchema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const serviceSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+        
+    },
+    price:{
+        type:Number,
+        required:true,
+    }
+   
+})
+
+const service = mongoose.model("service",serviceSchema);
+export default service;
