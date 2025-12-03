83fc40f3aa35fcad2cf089bedf642fa56a26236e
Rahinmon2903 <rahin2903@gmail.com>
2025-12-03 09:52:28 +0530

    booking schema added model completed

---
+++ Model/bookingSchema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+
+const bookingSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true,
+    },
+    service:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"service",
+
+        required:true,
+        
+    },
+    data:{
+        type:Date,
+        required:true,
+    }
+   
+})
+
+const booking = mongoose.model("booking",bookingSchema);
+export default booking;
